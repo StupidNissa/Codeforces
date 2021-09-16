dc9fb9222403e2a0a12481e15b4b15ad560059be
StupidNissa <foxsin1baan@gmail.com>
2021-09-16 19:21:15 +0530

    Codeforces#742

---
+++ Codeforces#742/C.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define int long long int
+#define mod 1000000007
+#define inf 1e18+42
+#define endl "\n"
+#define pi 3.1415926535897932384626433832795028841971693993751058
+#define maxn 100005
+
+#define out1(a) cout<<#a<<" "<<a<<endl
+#define out2(a,b) cout<<#a<<" "<<a<<" "<<#b<<" "<<b<<endl
+#define out3(a,b,c) cout<<#a<<" "<<a<<" "<<#b<<" "<<b<<" "<<#c<<" "<<c<<endl
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repr(i,a,b) for(int i=a;i>=b;i--)
+#define fori(it,A) for(auto it=A.begin();it!=A.end();it++)
+
+#define ft first
+#define sd second
+#define pb push_back
+#define mp make_pair
+#define pq priority_queue
+#define all(x) (x).begin(),(x).end()
+#define zero(x) memset(x,0,sizeof(x));
+#define ceil(a,b) (a+b-1)/b
+ 
+using namespace std;
+
+int binpow(int a, int b) {
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+//START OF CODE ->->->->->->->
+
+void solve()
+{
+    string s;
+    cin>>s;
+    vector<int> a;
+    rep(i,0,s.length()){
+        a.pb(s[i] - '0');
+    }
+    int ans = 0;
+    int x = 0;
+    int y = 0;
+    for ( int i = 0 ; i < a.size() ; i += 2 ){
+        x *= 10;
+        x += a[i];
+    }
+    for ( int i = 1 ; i < a.size() ; i += 2 ){
+        y *= 10;
+        y += a[i];
+    }
+
+    // out2(x,y);
+    ans += (x+1)*(y+1);
+    cout<<ans-2<<endl;
+}
+
+//END OF CODE ->->->->->->->->
+
+signed main()
+{
+    fast;
+    int t = 1;
+    cin>>t;
+    for ( int i = 1 ; i <= t ; i++ ){
+        // cout<<"Case #"<<i<<": ";
+        solve();
+    }
+    return 0;
+}
+
